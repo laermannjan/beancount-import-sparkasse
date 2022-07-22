5ec53b4b2b3d1b93b13254c1d518a49f3f0e018c
Jan Laermann <laermannjan@gmail.com>
2022-07-22 13:24:31 +0200

    Importer Protocol satisfied - Sparkasse CSV-CAMT

--- beancount_import_sparkasse/giro.py
+++ beancount_import_sparkasse/giro.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
 import csv
+import re
 from collections import OrderedDict
 from dataclasses import dataclass, field
 from datetime import datetime
+from typing import Sequence
 
 from beancount.core.amount import Amount
 from beancount.core.data import EMPTY_SET, Decimal, Posting, Transaction, new_metadata
@@ -13,7 +15,7 @@ DEFAULT_FIELDS = OrderedDict(
     account="Auftragskonto",
     booking_date="Buchungstag",
     value_date="Valutadatum",
-    posting_text="Buchungstext",
+    posting_type="Buchungstext",
     reference="Verwendungszweck",
     creditor="Glaeubiger ID",
     mandate_reference="Mandatsreferenz",
@@ -21,9 +23,9 @@ DEFAULT_FIELDS = OrderedDict(
     bulk_reference="Sammlerreferenz",
     debit_charge_original_amount="Lastschrift Ursprungsbetrag",
     reimbursement_return_debit="Auslagenersatz Ruecklastschrift",
-    payee="Beguenstigter/Zahlungspflichtiger",
-    iban="Kontonummer/IBAN",
-    bic="BIC (SWIFT-Code)",
+    payee_name="Beguenstigter/Zahlungspflichtiger",
+    payee_iban="Kontonummer/IBAN",
+    payee_bic="BIC (SWIFT-Code)",
     amount="Betrag",
     currency="Waehrung",
     info="Info",
@@ -40,6 +42,17 @@ class SparkasseCSVCAMTImporter(ImporterProtocol):
     date_format: str = "%d.%m.%y"
     file_encoding: str = "ISO-8859-1"
     fields: OrderedDict[str, str] = field(default_factory=lambda: DEFAULT_FIELDS)
+    meta_payee_iban: bool = True
+    meta_via: bool = True
+    meta_order_id: bool = True
+    meta_posting_type: bool = True
+    clean_strings: bool = True
+    payee_sumup_regex: str = r"SumUp \.(\w.*//.+/.+)/\d+"
+    ref_paypal_regex: str = r"Ihr Einkauf bei (\w+)"
+    ref_amazon_order_regex: str = r"(\w\d{2}-\d{7}-\d{7}) (\w.+) \w{16}"
+    ref_excluded_literal: Sequence[str] = ()
+    ref_excluded_regex: Sequence[str] = ()
+    add_dummy_expense: bool = True
 
     def __post_init__(self):
         self.expected_header = ";".join(
@@ -48,18 +61,57 @@ class SparkasseCSVCAMTImporter(ImporterProtocol):
 
     def _fmt_amount(self, amount: str) -> Decimal:
         """Removes German thousands separator and converts decimal point to US."""
-        return Decimal(amount.replace(",", "").replace(".", ","))
+        return Decimal(amount.replace(".", "").replace(",", "."))
 
-    def name(self):
-        """Return a unique id/name for this importer.
+    def _parse_paypal(self, txn) -> None:
+        """Retrieves the payee/debitor behind a PayPal transaction.
 
-        Returns:
-          A string which uniquely identifies this importer.
+        Checks whether the stated payee is "PayPal".
+        If the transaction reference also contains the
+        `self.paypal_reference_identifier` string, the actual
+        payee/debitor is the next string slice.
+
+        This works as paypal usually uses standardized references like:
+        "somehting . PP 3925 PP somthing your order at payee_name"
+        In the same case and if `self.meta_via` is True, a "via" meta tag
+        will also be added, noting that the transaction was wired via paypal.
         """
-        cls = self.__class__
-        return "{}.{}".format(cls.__module__, cls.__name__)
+        if txn["payee"].startswith("PayPal"):
+            match = re.search(self.ref_paypal_regex, txn["narration"])
+            if match:
+                if self.meta_via:
+                    txn["meta"]["via"] = "paypal"
+                txn["payee"] = match.group(1).strip()
+                txn["narration"] = ""
 
-    __str__ = name
+    def _parse_sumup(self, txn) -> None:
+        match = re.search(self.payee_sumup_regex, txn["payee"])
+        if match:
+            if self.meta_via:
+                txn["meta"]["via"] = "sumup"
+            txn["payee"] = match.group(1)
+            txn["narration"] = ""
+
+    def _parse_amazon(self, txn) -> None:
+        """Parse referece for order id and amazon name"""
+        match = re.search(self.ref_amazon_order_regex, txn["narration"])
+        if match:
+            txn["meta"]["order_number"] = match.group(1)
+            txn["payee"] = match.group(2)
+
+    def _excluded_narration_maybe(self, txn) -> None:
+        for excluded_str in self.ref_excluded_literal:
+            if excluded_str.lower() in txn["narration"].lower():
+                txn["narration"] = ""
+                return
+
+        for excluded_regex in self.ref_excluded_regex:
+            if re.search(excluded_regex, txn["narration"]):
+                txn["narration"] = ""
+                return
+
+    def _clean(self, s: str) -> str:
+        return " ".join(s.split())
 
     def identify(self, file) -> bool:
         """Return true if this importer matches the given file.
@@ -69,7 +121,7 @@ class SparkasseCSVCAMTImporter(ImporterProtocol):
         Returns:
           A boolean, true if this importer can handle this file.
         """
-        with open(file, encoding=self.file_encoding) as f:
+        with open(file.name, encoding=self.file_encoding) as f:
             header = f.readline().strip()
         return header.lower() == self.expected_header.lower()
 
@@ -95,7 +147,7 @@ class SparkasseCSVCAMTImporter(ImporterProtocol):
           A list of new, imported directives (usually mostly Transactions)
           extracted from the file.
         """
-        with open(file, encoding=self.file_encoding) as f:
+        with open(file.name, encoding=self.file_encoding) as f:
             transactions = csv.DictReader(
                 f, fieldnames=list(self.fields.keys()), delimiter=";", quotechar='"'
             )
@@ -104,29 +156,65 @@ class SparkasseCSVCAMTImporter(ImporterProtocol):
                 if i == 0:
                     # skip header
                     continue
-                meta = new_metadata(filename=file.name, lineno=i + 1)
-                date = datetime.strptime(txn["booking_date"], self.date_format).date()
-                amount = Amount(
-                    number=self._fmt_amount(txn["amount"]),
-                    currency=txn["currency"],
-                )
-                extracted_txn = Transaction(
-                    meta=meta,
-                    date=date,
-                    flag=self.FLAG,
-                    payee=txn["payee"],
+
+                # base values
+                txn_kwargs = dict(
+                    meta=dict(),
+                    date=datetime.strptime(
+                        txn["booking_date"], self.date_format
+                    ).date(),
+                    payee=txn["payee_name"],
                     narration=txn["reference"],
+                )
+
+                # augmentation
+                if self.meta_payee_iban and txn["payee_iban"]:
+                    txn_kwargs["meta"]["payee_iban"] = txn["payee_iban"]
+
+                if self.payee_sumup_regex:
+                    self._parse_sumup(txn=txn_kwargs)
+
+                if self.ref_paypal_regex:
+                    self._parse_paypal(txn=txn_kwargs)
+
+                if self.ref_amazon_order_regex:
+                    self._parse_amazon(txn=txn_kwargs)
+
+                self._excluded_narration_maybe(txn=txn_kwargs)
+
+                if self.meta_posting_type and txn["posting_type"]:
+                    txn_kwargs["meta"]["posting_type"] = txn["posting_type"]
+
+                extracted_txn = Transaction(
+                    meta=new_metadata(
+                        filename=file.name, lineno=i + 1, kvlist=txn_kwargs["meta"]
+                    ),
+                    date=txn_kwargs["date"],
+                    flag=self.FLAG,
+                    payee=self._clean(txn_kwargs["payee"]),
+                    narration=self._clean(txn_kwargs["narration"]),
                     tags=EMPTY_SET,
                     links=EMPTY_SET,
                     postings=[
                         Posting(
                             account=self.account,
-                            units=amount,  # type: ignore
+                            units=Amount(
+                                number=self._fmt_amount(txn["amount"]),
+                                currency=txn["currency"],
+                            ),
                             cost=None,
                             price=None,
                             flag=None,
                             meta=None,
-                        )
+                        ),
+                        Posting(
+                            account="Expenses:Dummy",
+                            units=None,
+                            cost=None,
+                            price=None,
+                            flag=None,
+                            meta=None,
+                        ),
                     ],
                 )
                 extracted_transactions.append(extracted_txn)
@@ -147,6 +235,7 @@ class SparkasseCSVCAMTImporter(ImporterProtocol):
         Returns:
           The name of the account that corresponds to this importer.
         """
+        return self.account
 
     def file_name(self, file):
         """A filter that optionally renames a file before filing.
@@ -161,6 +250,10 @@ class SparkasseCSVCAMTImporter(ImporterProtocol):
         Returns:
           The tidied up, new filename to store it as.
         """
+        match = re.search(r"\d{8}-(\d{7})-umsatz", file.name)
+        if match:
+            return f"{match.group(1)}.camt.csv"
+        return None
 
     def file_date(self, file):
         """Attempt to obtain a date that corresponds to the given file.
@@ -172,3 +265,4 @@ class SparkasseCSVCAMTImporter(ImporterProtocol):
           (If no date is returned, the file creation time is used. This is the
           default.)
         """
+        return max(map(lambda entry: entry.date, self.extract(file)))
--- tests/test_giro.py
+++ tests/test_giro.py
@@ -15,10 +15,34 @@ def csv_camt_file(tmp_path):
 
     rows = [
         header,
+        # excluded_literal debitk.0
         '"DE12345678901234567890";"01.01.99";"01.01.99";"KARTENZAHLUNG";\
         "1998-12-29T19:13 Debitk.0 2003-12 ";"";"";"28374929581093855282838499";\
-        "";"";"";"EDEKA THAUT, BERLIN//BERLIN/DE";"DE23394949202952160028";"\
+        "";"";"";"EDEKA THAUT, BERLIN//BERLIN/DE";"DE20293852183742160028";"\
         EDEKDEHHXXX";"-23,91";"EUR";"Umsatz gebucht"',
+        # audible + order_id
+        '"";"02.07.20";"02.07.20";"FOLGELASTSCHRIFT";\
+        "D01-4242420-1337337 Audible Gmbh AKCOCLWMGENLCLDT ";"DE32ZNENCL21039369";\
+        "6-)bI2039420mdbajdkdneGejl9vd(";"AKCOCLWMGENLCLDT";"";"";"";"AUDIBLE GMBH";\
+        "DE04300308800700054004";"TUBDDEDD";"-9,95";"EUR";"Umsatz gebucht"',
+        # paypal + via
+        '"";"03.07.20";"03.07.20";"FOLGELASTSCHRIFT";\
+        "PP.4994.PP . URBANSPORTS, Ihr Einkauf bei URBANSPORTS ";\
+        "LU96ZZZ0000000000000000058";"ALENPGHL024NX";"2095261239852 PP.4994.PP PAYPAL";\
+        "";"";"";"PayPal (Europe) S.a.r.l. et Cie., S.C.A.";\
+        "DE83920275324985729423";"DEUTDEFFXXX";"-49,00";"EUR";"Umsatz gebucht"',
+        # amazon _ order id
+        '"";"16.07.20";"16.07.20";"FOLGELASTSCHRIFT";\
+        "304-4242420-1337337 Amazon.de 231GADENOCKDLWWU ";"DE24ZZZ00000561652";\
+        "6-)bI8NO,k2U4Thaldkvn#dAGE9vd(";"231GADENOCKDLWWU";"";"";"";"\
+        AMAZON EU S.A R.L., NIEDERLASSUNG DEUTSCHLAND";"DE22398221409871234982";\
+        "TUBDDEDD";"-56,04";"EUR";"Umsatz gebucht"',
+        # amazon prime + order_id
+        '"";"16.07.20";"16.07.20";"FOLGELASTSCHRIFT";\
+        "D01-4242420-1337337 AMZNPrime DE FG20CJENCOIFKLEO ";"DE24AJDK2820223552";\
+        "6-)bI20923ldkljlamnenvalyl9vd(";"FG20CJENCOIFKLEO";"";"";"";\
+        "AMAZON EU S.A R.L., NIEDERLASSUNG DEUTSCHLAND";"DE20928012348723495336";\
+        "TUBDDEDD";"-34,00";"EUR";"Umsatz gebucht"',
     ]
     csv_file.write_text("\n".join(rows))
     return csv_file
@@ -36,9 +60,38 @@ def test_identify(default_importer, csv_camt_file):
     assert default_importer.identify(csv_camt_file)
 
 
-def test_extract(default_importer, csv_camt_file):
+def test_extract_date(default_importer, csv_camt_file):
     extracted_transactions = default_importer.extract(csv_camt_file)
     assert (
         extracted_transactions[0].date
         == datetime.strptime("01.01.99", "%d.%m.%y").date()
     )
+
+
+def test_extract_exclude_literal(default_importer, csv_camt_file):
+    extracted_transactions = default_importer.extract(csv_camt_file)
+    assert extracted_transactions[0].narration == ""
+
+
+def test_extract_parse_paypal(default_importer, csv_camt_file):
+    extracted_transactions = default_importer.extract(csv_camt_file)
+    assert extracted_transactions[2].payee == "URBANSPORTS"
+    assert extracted_transactions[2].meta["via"] == "paypal"
+
+
+def test_extract_parse_audible(default_importer, csv_camt_file):
+    extracted_transactions = default_importer.extract(csv_camt_file)
+    assert extracted_transactions[1].payee == "Audible Gmbh"
+    assert extracted_transactions[1].narration == "order_id: D01-4980510-3762206"
+
+
+def test_extract_parse_amazon(default_importer, csv_camt_file):
+    extracted_transactions = default_importer.extract(csv_camt_file)
+    assert extracted_transactions[3].payee == "Amazon.de"
+    assert extracted_transactions[3].narration == "order_id: 304-4813206-9870721"
+
+
+def test_extract_parse_amazon_prime(default_importer, csv_camt_file):
+    extracted_transactions = default_importer.extract(csv_camt_file)
+    assert extracted_transactions[4].payee == "AMZNPrime DE"
+    assert extracted_transactions[4].narration == "order_id: D01-6623628-2319009"
